f99b7675b6751a38e3d07ccbfda1e639e60ddce7
carlosfernandez <prof.carlos.tessier@gmail.com>
2017-02-11 15:16:47 +0100

    finished problem 5

--- problema6/cadenas.c
+++ problema6/cadenas.c
@@ -14,18 +14,20 @@ char * copyString (char * string){
 	return sCopy;
 }
 
+
+
 //pasa a mayúsculas todos los caracteres de la cadena
-	char * upperCase(char * string){
+char * upperCase(char * string){
 	char * sUpperCase;
 	int i = 0;
 
 	sUpperCase = copyString(string);
 
 	while(sUpperCase[i])
-   {
+  	{
       sUpperCase[i] = (char)toupper(sUpperCase[i]);
       i++;
-   }
+  	}
 
 	return sUpperCase;
 
@@ -35,21 +37,51 @@ char * copyString (char * string){
 char * lowerCase(char * string){
 
 	char * sLowerCase;
+	int i = 0;
 
 	sLowerCase = copyString(string);
+
+	while(sLowerCase[i])
+  	{
+      sLowerCase[i] = (char)tolower(sLowerCase[i]);
+      i++;
+  	}
 
 	return sLowerCase;
 
 }
 //realiza el palíndromo de la cadena de caracteres (escribe todos los caracteres al revés)
-//TODO
 char * palindrome(char * string){
 	char * sPalindrome;
+
 	sPalindrome = copyString(string);
+
+	int i = 0, j = 0;
+	char temp;
+    j = strlen(sPalindrome) - 1;
+ 
+   	while (i < j) {
+      temp = sPalindrome[i];
+      sPalindrome[i] = sPalindrome[j];
+      sPalindrome[j] = temp;
+      i++;
+      j--;
+
+   	}
+
 	return sPalindrome;
 }
 //escribe una palabra por línea.
-//TODO
 void writeWords(char * string){
+  char * token;
+  char * delim = " ,.-()";
+  // divide un string en una serie de tokens usando el delimitador delim.
+  token = strtok (string,delim);
+  printf("Palabras: \n")
+  while (token != NULL)
+  {
+    printf ("%s\n",token);
+    token = strtok (NULL, delim);
+  }
 	
 }
